e652c94b14c28d1e213168639a2b763fc02a0c07
Dj-Verstka <samping2money@gmail.com>
2022-09-05 14:52:19 +0700

    oksy - часть изображений - webp

--- sites/2_oksy-matthews/js/main.js
+++ sites/2_oksy-matthews/js/main.js
@@ -46,27 +46,22 @@ $(document).ready(function () {
 			$(this).children('.spoiler-body').fadeIn(500);
 		}
 	});
-	/*
-	$(function () {
-		if (navigator.userAgent.indexOf('Safari') != -1 &&
-			navigator.userAgent.indexOf('Chrome') == -1) {
-			$("body").addClass("safari");
-		}
-	});*/
 
 	function ThisIsWebP() {
 		var def = $.Deferred(), crimg = new Image();
 		crimg.onload = function () { def.resolve(); };
 		crimg.onerror = function () { def.reject(); };
-		crimg.src = "https://simpl.info/webp/cherry.webp";
+		crimg.src = "img/about-bg1.webp";
 		return def.promise();
 	}
 
 	ThisIsWebP().then(function () {
 		//Есть поддержка webp
 		console.log('есть');
+		$('body').addClass('webp-support');
 	}, function () {
 		//Нет поддержки webp
 		console.log('net');
+		$('body').addClass('webp-missed');
 	});
 });
