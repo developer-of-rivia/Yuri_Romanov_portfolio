ec9b917da4f8c1d36327174581bed19e7e9bdb31
Dj-Verstka <samping2money@gmail.com>
2022-09-14 14:43:36 +0700

    ef - контрольная точка

--- sites/3_ef/portfolio-interface/pi.js
+++ sites/3_ef/portfolio-interface/pi.js
@@ -8,3 +8,20 @@ $('.yuchoise__item--mobile').on('click', function () {
 $('.yuchoise__item--desk').on('click', function () {
     $('.yudevices').removeClass('yudevices--active');
 });
+
+
+// lazy load portfolio interface
+window.addEventListener("click", piframe);
+
+function piframe() {
+    const piElement = document.querySelector('.piframe');
+
+    if (!piElement.classList.contains('pi_loaded')) {
+        const piElementUrl = piElement.dataset.mobile;
+        piElement.insertAdjacentHTML(
+            "beforeend",
+            `<iframe src="${piElementUrl}" style="border: 0" allowfullscreen=""></iframe>`
+        );
+        piElement.classList.add('pi_loaded');
+    }
+}
